e1e44f89f5536ed4df725436f5c265a48ad546ef
Matthew Zimmerman <Zimmerman@Matthews-MacBook-Pro.local>
2012-12-20 23:07:17 -0500

    Initial Commit

---
+++ FireplaceReceiver/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FireplaceReceiver
+//
+//  Created by Matthew Zimmerman on 12/20/12.
+//  Copyright (c) 2012 Matthew Zimmerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
